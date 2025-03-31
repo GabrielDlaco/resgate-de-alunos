1e43a4b20ae35809ed94e1b5046db4319f1f14f9
Gabriel Laco <gabriel.laco@aluno.senai.br>
2025-03-31 10:14:54 -0300

    Atualizando

--- js/app.js
+++ js/app.js
@@ -2,11 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menu-btn')
     const navMenu = document.getElementById('nav-menu')
 
+    function verificarTamanhoDoNavegador() {
+        if(window.innerWidth > 575){
+            navMenu.style.display = "none";
+        } else{
+            navMenu.style.display = "flex";
+        }
+    }
+
     menuBtn.addEventListener('click', ()=>{
         if(navMenu.style.display === 'flex'){
             navMenu.style.display = 'none'
         } else{
             navMenu.style.display = 'flex'
         }
+    })
+
+    window.addEventListener('resize', () => {
+        verificarTamanhoDoNavegador()
     })
 })
